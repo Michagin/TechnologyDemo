2c165a41b3b22a8d8bafdddf2339ae5d650f47dd
AndreasMichagin <andreasmichagin@gmail.com>
2021-02-08 15:42:47 +0100

    added calc endpoint

--- src/main/java/kea/dat20b/technology/controller/FirstController.java
+++ src/main/java/kea/dat20b/technology/controller/FirstController.java
@@ -2,7 +2,10 @@ package kea.dat20b.technology.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Locale;
 
 @Controller
 public class FirstController {
@@ -11,5 +14,11 @@ public class FirstController {
     @ResponseBody
     public String landing(){
         return "<b>index</b>";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody
+    public String hello(@RequestParam int input){
+        return String.valueOf(input*input);
     }
 }
