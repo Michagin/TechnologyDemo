bd002e212cc27add5b4307da67d40d22bfe4c83d
AndreasMichagin <andreasmichagin@gmail.com>
2021-02-08 15:55:53 +0100

    added calc controller and multiplier function

---
+++ src/main/java/kea/dat20b/technology/Calculator.java
@@ -0,0 +1,8 @@
+package kea.dat20b.technology;
+
+public class Calculator {
+
+    public int multiply(int x, int y){
+        return x*y;
+    }
+}
--- src/main/java/kea/dat20b/technology/controller/CalculatorController.java
+++ src/main/java/kea/dat20b/technology/controller/CalculatorController.java
@@ -1,18 +1,19 @@
 package kea.dat20b.technology.controller;
 
+import kea.dat20b.technology.Calculator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Locale;
-
 @Controller
-public class FirstController {
+public class CalculatorController {
 
-    @GetMapping("/")
+    @GetMapping("/calc")
     @ResponseBody
-    public String landing(){
-        return "<b>index</b>";
+    public String calulator(@RequestParam int x, int y){
+        Calculator calc = new Calculator();
+        return String.valueOf(calc.multiply(x,y));
     }
 }
